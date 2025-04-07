a3116e8402fb9e0fe6b2c081272d3c8db28f2a00
Nekel Mitchell <111768636+nekelmitchell@users.noreply.github.com>
2025-04-07 03:41:25 +0000

    Ready to Grade

---
+++ hello-nekel/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter your name: ");
+string firstname = Console.ReadLine();
+Console.WriteLine($"Hello, {firstname}!");
